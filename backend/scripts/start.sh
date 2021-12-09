3198e13d00b7a4c6fca445c5fb504c28d07303dd
A-J Roos <asjas@hey.com>
2021-12-09 10:22:56 +0200

    update start script

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Start the first process
 ./node_modules/.bin/keystone prisma migrate deploy
 
-# Start the second process
 ./node_modules/.bin/keystone start &
 
-# Wait for any process to exit
 wait -n
 
-# Exit with status of process that exited first
 exit $?
